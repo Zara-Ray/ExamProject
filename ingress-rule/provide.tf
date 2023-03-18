8226c038b151ae09fd2de38f1760447427b2e5de
Chizaram Darius-Mary Ezugwu <100358590+Zara-Ray@users.noreply.github.com>
2023-03-18 13:25:18 +0100

    Update provide.tf

--- ingress-rule/provide.tf
+++ ingress-rule/provide.tf
@@ -7,20 +7,20 @@ terraform {
   }
 }
 
-data "aws_eks_cluster" "demo" {
-  name = "demo"
+data "aws_eks_cluster" "zaltsch" {
+  name = "zaltsch"
 }
-data "aws_eks_cluster_auth" "demo_auth" {
-  name = "demo_auth"
+data "aws_eks_cluster_auth" "zaltsch_auth" {
+  name = "zaltsch_auth"
 }
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.demo.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.zaltsch.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.zaltsch.certificate_authority[0].data)
   version          = "2.16.1"
   config_path = "~/.kube/config"
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "demo"]
+    args        = ["eks", "get-token", "--cluster-name", "zaltsch"]
     command     = "aws"
   }
 }
